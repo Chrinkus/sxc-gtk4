ddb4bb331fc2af33018ad721b9d8f1b86c874ab1
Chris Schick <seeschickrun@gmail.com>
2022-01-03 13:40:49 -0600

    Part 3 complete!
    All the scaffolding is making adding elements a breeze. There is a LOT
    of GLib in here. Will need to review the API docs.

--- src/example-5/example_app_win.c
+++ src/example-5/example_app_win.c
@@ -3,6 +3,7 @@
 
 struct _ExampleAppWindow {
 	GtkApplicationWindow parent;
+	GtkWidget* stack;
 };
 
 G_DEFINE_TYPE(ExampleAppWindow, example_app_window,
@@ -17,6 +18,8 @@ static void example_app_window_class_init(ExampleAppWindowClass* class)
 {
 	gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class),
 			"/org/gtk/exampleapp/window.ui");
+	gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class),
+			ExampleAppWindow, stack);
 }
 
 ExampleAppWindow* example_app_window_new(ExampleApp* app)
@@ -26,6 +29,29 @@ ExampleAppWindow* example_app_window_new(ExampleApp* app)
 
 void example_app_window_open(ExampleAppWindow* win, GFile* file)
 {
-	// Nothing yet..
+	char* basename = g_file_get_basename(file);
+
+	GtkWidget* scrolled = gtk_scrolled_window_new();
+	gtk_widget_set_hexpand(scrolled, TRUE);
+	gtk_widget_set_vexpand(scrolled, TRUE);
+
+	GtkWidget* view = gtk_text_view_new();
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(view), FALSE);
+	gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(view), FALSE);
+	gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled), view);
+	gtk_stack_add_titled(GTK_STACK(win->stack), scrolled, basename,
+			basename);
+
+	char* contents;
+	gsize length;
+	if (g_file_load_contents(file, NULL, &contents, &length, NULL, NULL)) {
+		GtkTextBuffer* buffer;
+
+		buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
+		gtk_text_buffer_set_text(buffer, contents, length);
+		g_free(contents);
+	}
+
+	g_free(basename);
 }
 
