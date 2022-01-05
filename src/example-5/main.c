069464107dcc052f81b518c0bec9e634412909f9
Chris Schick <seeschickrun@gmail.com>
2022-01-04 22:21:13 -0600

    Added CMake command to generate schema locally
    I missed the added line in main.c that helps the app find the schema.

--- src/example-5/main.c
+++ src/example-5/main.c
@@ -4,6 +4,11 @@
 
 int main(int argc, char* argv[])
 {
+	/* During development the schema will be installed in the
+	 * build directory. This line tells GTK where to find it.
+	 */
+	g_setenv("GSETTINGS_SCHEMA_DIR", ".", FALSE);
+
 	return g_application_run(G_APPLICATION(example_app_new()), argc, argv);
 }
 
